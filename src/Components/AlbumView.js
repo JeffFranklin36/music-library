1e69f5355c368646917db8236f7a3b6a24ed80a7
Jeffrey <jeff.1001@live.com>
2022-06-23 19:43:48 -0500

    fleshed out artist and album views

--- src/Components/AlbumView.js
+++ src/Components/AlbumView.js
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function AlbumView(props){
  const { id } = useParams()
  const [albumData, setAlbumData] = useState([])
 
+
+ useEffect(() => {
+    const API_URL = `http://localhost:4000/song/${id}`
+    const fetchData = async () => {
+      const response = await fetch(API_URL)
+      const data = await response.json()
+      console.log(data)
+      setAlbumData(data.results)
+    }
+
+    fetchData()
+ }, [id])
+
+ const songs = albumData.filter(data => data.wrapperType === 'track')
+
+ const display = songs.map(song => {
+  return (
+    <div key={song.trackId}>
+        <p>{song.trackName}</p>
+    </div>
+  )
+ })
+
  return (
   <div>
-   <h2>The id passed was: {id}</h2>
-   <p>Album Data Goes Here!</p>
+    {display}
   </div>
   )
 }
